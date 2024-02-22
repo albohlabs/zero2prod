c3c5c21e4d3a1cd2282204a9debb05a6c3504267
alboh <github@alboh.de>
2024-02-22 11:04:14 +0100

    provide sqlx connection

--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,21 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
+
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::get().to(subscribe))
+            .route("/subscriptions", web::post().to(subscribe))
+            // Get a pointer copy and attach it to the application state
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
