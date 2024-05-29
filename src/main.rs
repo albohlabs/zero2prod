46baa5c9de434aa892b36ee59c66d2b2203d3d30
alboh <github@alboh.de>
2024-05-29 21:37:59 +0200

    refactor: extract startup code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
@@ -11,20 +8,6 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
 
     // Here we choose to bind explicitly to localhost, 127.0.0.1, for security
     // reasons. This binding may cause issues in some environments. For example,
@@ -32,12 +15,9 @@ async fn main() -> std::io::Result<()> {
     // server when it is bound to WSL2's localhost interface. As a workaround,
     // you can choose to bind to all interfaces, 0.0.0.0, instead, but be aware
     // of the security implications when you expose the server on all interfaces.
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool, email_client)?.await?;
+
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
 
     Ok(())
 }
